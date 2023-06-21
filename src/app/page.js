9009596e30d7770c31661d9c1b54fbb7429652a0
Jesus Carmona <ryzen3000@outlook.es>
2023-06-21 09:26:53 -0600

    Cambios en el Codigo

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <div>
       <div className={styles.description}>
         <p>
           Get started by editing &nbsp;
@@ -40,21 +40,9 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <h1> CONTENIDO NUEVO</h1>
-      </div>
+  
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -66,36 +54,14 @@ export default function Home() {
             Learn <span>-&gt;</span>
           </h2>
           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
           <p>Explore the Next.js 13 playground.</p>
         <p> Prueba de Cambios</p>
         </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Modo Sexo Activado
-           jajjajajaja
-          </p>
-        </a>
+          
+  
+
       </div>
-    </main>
+</div>
   )
 }
