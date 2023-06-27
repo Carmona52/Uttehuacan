34abee4b9e38c7d6da52b018be411ee9185d666d
Jesus Carmona <ryzen3000@outlook.es>
2023-06-27 10:35:59 -0600

    Creacion del Login

---
+++ src/app/login/page.js
@@ -0,0 +1,19 @@
+export default function Home() {
+    return (
+     <div>
+        <form>
+            <input type="text">
+
+            </input>
+
+            <input type="password">
+                
+            </input>
+
+            <button action="submit"></button>
+
+        </form>
+     </div>
+    )
+  }
+  
---
+++ src/app/profile/page.js
@@ -0,0 +1,6 @@
+export default function Home() {
+    return (
+     <div>SOY PROFILE     </div>
+    )
+  }
+  
