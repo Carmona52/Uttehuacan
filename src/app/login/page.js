428fdfa64548a6decf390a356f01506c2cb61afe
Jesus Carmona <ryzen3000@outlook.es>
2023-06-28 07:58:48 -0600

    Cambios Generales

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,18 +2,18 @@ export default function Home() {
     return (
      <div style={{
         display: "flex",
-        flexDirection:"column",
-        widt:"500px"
+        flexdirection:"column",
+        width:"500px"
      }}>
         <form>
             <input type="text" placeholder="INSERTE SU USUARIO">
 
-            </input>
+            </input> <br></br> <br></br>
 
             <input type="password" placeholder="INSERTE LA CONTRASEÑA">
-                
-            </input>
-
+                 
+            </input> <br></br> <br></br>
+ 
             <button action="submit">BOTON DE ENVIAR</button>
 
         </form>
