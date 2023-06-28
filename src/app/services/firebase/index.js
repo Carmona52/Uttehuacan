3e3936b2a79d85c3ff50dd8b5780e264405a7db5
Jesus Carmona <ryzen3000@outlook.es>
2023-06-28 09:16:40 -0600

    Firebase authentication

--- src/app/services/firebase/index.js
+++ src/app/services/firebase/index.js
@@ -22,3 +22,18 @@ const analytics = getAnalytics(app);
 
 
 export default app; 
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
